8e2e910c9f21f5970f6e12172c2ca571f9342bde
nassimbouziane <bouzianenassim17102002@gmail.com>
2023-04-17 11:09:39 +0200

    Api(): Adding the python api and a route that return the number of person

---
+++ api/api.py
@@ -0,0 +1,33 @@
+import hug
+import requests
+import os
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+import hug
+
+# # Add your Computer Vision subscription key and endpoint to your environment variables.
+# subscription_key = "eeaf3e3ab15b45f697be75bc5df5130b"
+# endpoint = "https://tic-clo2-vision.cognitiveservices.azure.com/vision/v3.2/analyze"
+
+# Instantiate a client object
+#computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+
+@hug.post('/image')
+def process_image(image):
+    print(image)
+    subscription_key = 'eeaf3e3ab15b45f697be75bc5df5130b'
+    endpoint = 'https://tic-clo2-vision.cognitiveservices.azure.com/vision/v3.2/analyze'
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+
+    # Envoie l'image à l'API Computer Vision
+    response = requests.post(endpoint, headers=headers, params={'visualFeatures': 'Objects'},
+                             files={'image': image})
+    response.raise_for_status()
+    analysis = response.json()
+
+    # Compte le nombre de personnes détectées dans l'image
+    objects = analysis['objects']
+    person_count = sum(obj['object'] == 'person' for obj in objects)
+
+    return {'person_count': person_count}
--- src/formScript.js
+++ src/formScript.js
@@ -3,6 +3,7 @@ window.onload = function() {
   const rangeValue = document.getElementById('range-value');
   const form = document.getElementById('my-form');
   const defaultRangeValue = rangeInput.value;
+  const input = document.querySelector('input[type="file"]');
 
 
   rangeInput.addEventListener('input', () => {
@@ -11,6 +12,19 @@ window.onload = function() {
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('image', input.files[0]);
+   fetch('http://localhost:8000/image', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.error(error));
+
+
     const textInput = document.getElementById('text-input').value;
     const rangeInput = document.getElementById('range-input').value;
     console.log(`Texte : ${textInput}, Valeur : ${rangeInput}`);
